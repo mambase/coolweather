50e027023ccf6fbb09bbc84a82f65d93baf763d5
hch <1024625657@qq.com>
2017-05-04 15:40:03 +0800

    完成遍历省市县三级列表的功能。

--- app/src/main/java/com/dianqu/android/coolweather/MainActivity.java
+++ app/src/main/java/com/dianqu/android/coolweather/MainActivity.java
@@ -1,7 +1,9 @@
 package com.dianqu.android.coolweather;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Fragment;
+import android.app.FragmentManager;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FragmentManager fm = getFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
+
+        if (fragment == null) {
+            fragment = new ChooseAreaFragment();
+            fm.beginTransaction()
+                    .add(R.id.fragment_container, fragment)
+                    .commit();
+        }
     }
 }
